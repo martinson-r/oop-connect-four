3f820a4b1a8ee6bb1f090e0d6845cd3fc9d53c55
james <monaco.james18@gmail.com>
2020-10-08 16:07:51 -0500

    working through chips falling

--- column.js
+++ column.js
@@ -10,6 +10,16 @@ export class Column {
     }
 
     getTokenAt() {
-
+      for (let i = 5; i >= 0; i--) {
+        if (this.tokens[i] === undefined) {
+          return null;
+        }
+        if (this.tokens[i] === 1) {
+          return 1;
+        }
+        if (this.tokens[i] === 2) {
+          return 2;
+        }
+      }
     }
 }
--- game.js
+++ game.js
@@ -1,8 +1,12 @@
+import { Column } from './column.js';
+// import { ConnectFour } from "./connect-four.js"
+
 export class Game {
     constructor(player1, player2) {
         this.player1 = player1;
         this.player2 = player2;
         this.currentPlayer = 1;
+        this.columns = [0, 1, 2, 3, 4, 5, 6];
     }
 
     getName() {
@@ -11,13 +15,20 @@ export class Game {
 
     playInColumn() {
 
-            if (this.currentPlayer === 1) {
+        if (this.currentPlayer === 1) {
 
-                return this.currentPlayer = 2;
-
-            } else {
+            return this.currentPlayer = 2;
+        } else {
             return this.currentPlayer = 1;
-            }
+        }
 
     }
+
+    clickTargets = document.getElementById('click-targets');
+        clickTargets.addEventListener("click", event => {
+            let currentColumn = clickTargets.slice(7);
+            console.log(currentColumn);
+        }
+
+
 }
