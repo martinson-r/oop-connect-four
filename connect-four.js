e187aee4ac3e6a365af2900057d1ebff9e1e0392
james <monaco.james18@gmail.com>
2020-10-08 13:00:11 -0500

    New game and player names added

--- connect-four.js
+++ connect-four.js
@@ -2,14 +2,41 @@ import { Game } from './game.js';
 
 let game = undefined;
 
+let updateUI = function () {
+    let boardHolder = document.getElementById("board-holder");
+    if (game === undefined) {
+        boardHolder.setAttribute("class", "is-invisible");
+    } else {
+        boardHolder.classList.remove("is-invisible");
+        let gameName = document.getElementById("game-name");
+        gameName.innerHTML = game.getName();
+    }
+
+}
+
 window.addEventListener("DOMContentLoaded", event => {
     let player2 = document.getElementById('player-2-name');
     let player1 = document.getElementById('player-1-name');
+    let newGame = document.getElementById('new-game');
     player1.addEventListener("keyup", event => {
-        let newGame = document.getElementById('new-game');
-        if (player1 && player2) {
+        if (player1.value && player2.value) {
             newGame.disabled = false;
+        } else {
+            newGame.disabled = true;
         }
-        newGame.disabled = true;
     });
+    player2.addEventListener("keyup", event => {
+        let newGame = document.getElementById('new-game');
+        if (player1.value && player2.value) {
+            newGame.disabled = false;
+        } else {
+            newGame.disabled = true;
+        }
+    });
+
+    newGame.addEventListener("click", event => {
+        game = new Game(player1.value, player2.value);
+        newGame.disabled = true;
+        updateUI();
+    })
 });
