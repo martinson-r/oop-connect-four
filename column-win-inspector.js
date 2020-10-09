3d2e81ff6dee9342118a2dfdbf281a3f193b70a7
Rihana Chang <martinson.r@gmail.com>
2020-10-09 15:21:04 -0700

    Formatting tweak

--- column-win-inspector.js
+++ column-win-inspector.js
@@ -16,10 +16,9 @@ export class ColumnWinInspector {
                 nextToken === nextToken1 &&
                 nextToken1 === nextToken2 &&
                 currentToken != null) {
-                //game.winnerNumber = currentToken;
+                game.winnerNumber = currentToken;
                 console.log(currentToken);
                 return currentToken;
-                //console.log(game.winnerNumber);
         }
         }
         console.log("nope!");
