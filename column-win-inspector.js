de09f02a38f1d42462549bbe5021b805df602056
Rihana Chang <martinson.r@gmail.com>
2020-10-09 13:48:17 -0700

    Added column win inspector

--- column-win-inspector.js
+++ column-win-inspector.js
@@ -1,15 +1,19 @@
-import { Game } from "./game";
+import { Game } from "./game.js";
 
 export class ColumnWinInspector {
     constructor (columnObj={}) {
         this.columnObj = columnObj;
     }
 
-    inspect() {
+    inspect(columnObj) {
         for (let i = 0; i <= 6; i++) {
-            if (columnObj[i].game.updateUI.playerChip) {
-
+            if (columnObj.game.updateUI.playerChip) {
+                console.log(game.updateUI.playerChip);
+                return game.updateUI.playerChip;
             }
         }
+        console.log("nope!");
+        return 0;
+
     }
 }
--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import { Column } from './column.js';
 // import { ConnectFour } from "./connect-four.js"
+import { ColumnWinInspector } from './column-win-inspector.js';
 
 export class Game {
     constructor(player1, player2) {
@@ -30,9 +31,27 @@ export class Game {
 
     }
 
+    checkForColumnWin() {
+        for ( let i = 0; i <= 6; i++) {
+            let columnToInspect = this.columns[i];
+            if (columnWinInspector.inspect(columnToInspect) === 1) {
+                this.winnerNumber = 1;
+            } else if (columnWinInspector.inspect(columnToInspect) === 2) {
+                this.winnerNumber = 2;
+        }
+    }
+}
+
     playInColumn(columnIndex) {
         this.columns[columnIndex].add(this.currentPlayer);
         this.checkForTie();
+
+        if (this.winnerNumber != 0) {
+            return false;
+        } else {
+            this.checkForColumnWin();
+        }
+
         if (this.currentPlayer === 1) {
             return this.currentPlayer = 2;
         } else {
