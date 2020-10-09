d58d41c2fff2f571c9b606e61f7d2ad286901015
Rihana Chang <martinson.r@gmail.com>
2020-10-09 14:46:01 -0700

    Column full functionality

--- column-win-inspector.js
+++ column-win-inspector.js
@@ -1,4 +1,5 @@
 import { Game } from "./game.js";
+import { Column } from "./column.js";
 
 export class ColumnWinInspector {
     constructor (columnObj) {
@@ -6,15 +7,20 @@ export class ColumnWinInspector {
     }
 
     inspect() {
-        for (let i = 0; i <= 6; i++) {
+        for (let i = 0; i <= 2; i++) {
             let currentToken = column.getTokenAtRow(i);
             let nextToken = column.getTokenAtRow(i + 1);
             let nextToken1 = column.getTokenAtRow(i + 2);
             let nextToken2 = column.getTokenAtRow(i + 3);
-            if (currentToken === nextToken && nextToken === nextToken1 && nextToken1 === nextToken2) {
-                game.winnerNumber = currentToken;
-                console.log(game.winnerNumber);
-            }
+            if (currentToken === nextToken &&
+                nextToken === nextToken1 &&
+                nextToken1 === nextToken2 &&
+                currentToken != null) {
+                //game.winnerNumber = currentToken;
+                console.log(currentToken);
+                return currentToken;
+                //console.log(game.winnerNumber);
+        }
         }
         console.log("nope!");
         return 0;
